fadbb9e65b04ff6a3f85ca7159474974c81fadcc
oliwia <oliwia.giercza@gmail.com>
2023-03-21 14:03:49 +0100

    button functionality and added another dice - need to learn emulator cause it's not working properly - keeps lagging and does stuff by itself?

--- app/src/main/java/com/example/app2_diceroller/MainActivity.kt
+++ app/src/main/java/com/example/app2_diceroller/MainActivity.kt
@@ -2,13 +2,41 @@ package com.example.app2_diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
 
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+        val diceRoll1 = dice1.roll()
+        val diceRoll2 = dice2.roll()
+        // Update the screen with the dice roll
+        val resultTextView1: TextView = findViewById(R.id.textView1)
+        resultTextView1.text = diceRoll1.toString()
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = diceRoll2.toString()
+    }
+}
+
+class Dice(private val sumSides: Int) {
+    fun roll(): Int {
+        return (1..sumSides).random()
     }
 }
