8b2b912fd36c1b4a94b62d4c646517ed8151023e
oliwia <oliwia.giercza@gmail.com>
2023-03-31 18:13:11 +0200

    change dice rolls in regards to the number generated with the button

--- app/src/main/java/com/example/app2_diceroller/MainActivity.kt
+++ app/src/main/java/com/example/app2_diceroller/MainActivity.kt
@@ -23,12 +23,19 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
-        val dice1 = Dice(6)
-        val diceRoll1 = dice1.roll()
-        // Update the screen with the dice roll
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
-
+        // Update the screen with the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
 
